f188ceeaaeed4046554ceea5d7c68ca327a0f78a
Niema Moshiri <niemamoshiri@gmail.com>
2018-05-10 17:55:06 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,61 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='treeswift',  # Required
+    version='1.0.0',  # Required
+    description='TreeSwift: Fast tree module for Python 2 and 3',  # Required
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/niemasd/TreeSwift',  # Optional
+    author='Niema Moshiri',  # Optional
+    author_email='niemamoshiri@gmail.com',  # Optional
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='tree phylogenetics fast',  # Optional
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    extras_require={  # Optional
+        'dev': ['check-manifest'],
+    },
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/niemasd/TreeSwift/issues',
+        'Source': 'https://github.com/niemasd/TreeSwift',
+    },
+)
