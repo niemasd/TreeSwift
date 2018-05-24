958e5b1b9444729cbdb4bdde8bd24186e84a5f94
Niema Moshiri <niemamoshiri@gmail.com>
2018-05-24 16:14:09 -0700

    Updated documentation

--- setup.py
+++ setup.py
@@ -12,17 +12,12 @@ from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='treeswift',  # Required
     version='1.0.27',  # Required
     description='TreeSwift: Fast tree module for Python 2 and 3',  # Required
-    long_description=long_description,  # Optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
+    long_description='TreeSwift is a Python library for parsing, manipulating, and iterating over (rooted) tree structures. TreeSwift places an emphasis on speed.',  # Optional
+    long_description_content_type='text/plain',  # Optional (see note above)
     url='https://github.com/niemasd/TreeSwift',  # Optional
     author='Niema Moshiri',  # Optional
     author_email='niemamoshiri@gmail.com',  # Optional
