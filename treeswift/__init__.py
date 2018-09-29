e4536505e75729f4efc43810b700a08c7f8e61a3
Niema Moshiri <niemamoshiri@gmail.com>
2018-09-29 12:22:54 -0400

    Fixed documentation

--- treeswift/__init__.py
+++ treeswift/__init__.py
@@ -1,3 +1,3 @@
 from treeswift.Tree import Tree,plot_ltt,read_tree,read_tree_dendropy,read_tree_newick,read_tree_nexml,read_tree_nexus
 from treeswift.Node import Node
-__all__ = ['Node','Tree','read_tree_newick','read_tree_nexml','read_tree_nexus']
+__all__ = ['Node','Tree','read_tree_newick','read_tree_newick','read_tree_nexml','read_tree_nexus']
