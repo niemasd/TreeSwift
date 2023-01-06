4d878e8dae941081ecf425ad5a2000ba415bd773
Niema Moshiri <niemamoshiri@gmail.com>
2023-01-06 08:13:43 -0800

    Updated supported Python versions
    * Python 2.6 is definitely no longer supported
    * Python 2.7 *might* be supported (haven't tested), but because Python 2 has been sunset, official support in TreeSwift is removed
    * Removed versions of Python 3 that are not actively supported by Python (https://www.python.org/downloads/)

--- setup.py
+++ setup.py
@@ -37,12 +37,12 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     keywords='tree phylogenetics fast',  # Optional
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
